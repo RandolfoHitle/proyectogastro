d75b034f94e5b26976e9a0438879f0a1876a1807
RandolfoHitle <eroldanp@miumg.edu.gt>
2024-10-20 20:00:17 -0600

    primera base

---
+++ recetas/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('', views.lista_recetas, name='lista_recetas'),  # Lista de recetas públicas
+    path('receta/<int:receta_id>/', views.detalle_receta, name='detalle_receta'),  # Detalle de receta
+    path('gestion/', views.gestion_recetas, name='gestion_recetas'),  # Vista de gestión privada
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
