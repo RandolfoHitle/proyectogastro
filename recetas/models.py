1382d13707b50663064c78df96b7d55f00f612ad
RandolfoHitle <eroldanp@miumg.edu.gt>
2024-10-24 23:16:28 -0600

    crear resetas

---
+++ recetas/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.nombre}'
+
+
+class Receta(models.Model):
+    titulo = models.CharField(max_length=200)
+    ingredientes = models.TextField()
+    instrucciones = models.TextField()
+    tiempo_preparacion = models.IntegerField()
+    categoria = models.ManyToManyField(Categoria)
+    imagen = models.ImageField(upload_to='recetas/')
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    valoracion = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return f'{self.titulo}'
+
