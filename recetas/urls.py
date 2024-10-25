7604ce04de3513fdd3c7a9048125311607d0a1e5
RandolfoHitle <eroldanp@miumg.edu.gt>
2024-10-24 23:41:22 -0600

    vistas recetas

--- recetas/urls.py
+++ recetas/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
-
+from .views import (
+    CategoriaListView, CategoriaCreateView, CategoriaUpdateView, CategoriaDeleteView,
+    RecetaListView, RecetaCreateView, RecetaUpdateView, RecetaDeleteView, RecetaDetailView
+)
 
 
 urlpatterns = [
-
+    # URLs para Categorias
+    path('categorias/', CategoriaListView.as_view(), name='categoria-list'),
+    path('categorias/nueva/', CategoriaCreateView.as_view(), name='categoria-create'),
+    path('categorias/editar/<int:pk>/', CategoriaUpdateView.as_view(), name='categoria-update'),
+    path('categorias/eliminar/<int:pk>/', CategoriaDeleteView.as_view(), name='categoria-delete'),
+    
+    # URLs para Recetas
+    path('recetas/', RecetaListView.as_view(), name='receta-list'),
+    path('recetas/nueva/', RecetaCreateView.as_view(), name='receta-create'),
+    path('recetas/editar/<int:pk>/', RecetaUpdateView.as_view(), name='receta-update'),
+    path('recetas/eliminar/<int:pk>/', RecetaDeleteView.as_view(), name='receta-delete'),
+    path('recetas/<int:pk>/', RecetaDetailView.as_view(), name='receta-detail'),
 ]
--- recetas/views.py
+++ recetas/views.py
@@ -1,1 +1,62 @@
 from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from .models import Categoria, Receta
+
+# Listar Categorias
+class CategoriaListView(ListView):
+    model = Categoria
+    template_name = 'categoria/categoria_list.html'
+    context_object_name = 'categorias'
+
+# Crear Categoria
+class CategoriaCreateView(CreateView):
+    model = Categoria
+    fields = ['nombre']
+    template_name = 'categoria/categoria_form.html'
+    success_url = reverse_lazy('categoria-list')
+
+# Editar Categoria
+class CategoriaUpdateView(UpdateView):
+    model = Categoria
+    fields = ['nombre']
+    template_name = 'categoria/categoria_form.html'
+    success_url = reverse_lazy('categoria-list')
+
+# Eliminar Categoria
+class CategoriaDeleteView(DeleteView):
+    model = Categoria
+    template_name = 'categoria/categoria_confirm_delete.html'
+    success_url = reverse_lazy('categoria-list')
+
+
+# Listar Recetas
+class RecetaListView(ListView):
+    model = Receta
+    template_name = 'receta/receta_list.html'
+    context_object_name = 'recetas'
+
+# Detalle de Receta
+class RecetaDetailView(DetailView):
+    model = Receta
+    template_name = 'receta/receta_detail.html'
+    context_object_name = 'receta'
+
+# Crear Receta
+class RecetaCreateView(CreateView):
+    model = Receta
+    fields = ['titulo', 'ingredientes', 'instrucciones', 'tiempo_preparacion', 'categoria', 'imagen', 'autor', 'valoracion']
+    template_name = 'receta/receta_form.html'
+    success_url = reverse_lazy('receta-list')
+
+# Editar Receta
+class RecetaUpdateView(UpdateView):
+    model = Receta
+    fields = ['titulo', 'ingredientes', 'instrucciones', 'tiempo_preparacion', 'categoria', 'imagen', 'autor', 'valoracion']
+    template_name = 'receta/receta_form.html'
+    success_url = reverse_lazy('receta-list')
+
+# Eliminar Receta
+class RecetaDeleteView(DeleteView):
+    model = Receta
+    template_name = 'receta/receta_confirm_delete.html'
+    success_url = reverse_lazy('receta-list')
