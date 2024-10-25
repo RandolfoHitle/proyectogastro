31c55a391cc30c19cb2338e10ed7690b00282cef
RandolfoHitle <eroldanp@miumg.edu.gt>
2024-10-25 11:11:22 -0600

    parte final

--- public/urls.py
+++ public/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from .views import HomeViewPublic, RecetasViewPublic, ContactoViewPublic, HistoriaViewPublic, RecetaDetailViewPublic
 
 urlpatterns = [
+    path('', HomeViewPublic.as_view(), name='homePublic'),
+    path('recetario/', RecetasViewPublic.as_view(), name='recetasPublic'),
+    path('recetario/<int:pk>/', RecetaDetailViewPublic.as_view(), name='receta_detailPublic'),
+    path('contacto/', ContactoViewPublic.as_view(), name='contactoPublic'),
+    path('historia/', HistoriaViewPublic.as_view(), name='historiaPublic'),
 
 ]
